b167d01a363ccdc48e0c7c7e644651b82df43d5a
anleaes <anleaes@gmail.com>
2025-03-14 21:13:10 -0300

    dsas

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     c1 = Categoria("informatica", "Produtos de informatica")
     c2 = Categoria("Alimentos", "Alimentos em geral")
     p1 = Produto("Iphone", "Telefone rico", "10/10/2025", True, c2)
+    print(p1._nome)
     
 if __name__ == "__main__": 
     main()
