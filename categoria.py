7d3978afd5507870cf986fed7cc85c1a879a8d46
miguel <mart@mail>
2025-03-14 21:07:51 -0300

    add desc em cat

--- categoria.py
+++ categoria.py
@@ -1,6 +1,7 @@
 class Categoria:
     def __init__(self, nome, descricao):
         self._nome = nome
+        self._descricao = descricao
 
     def __str__(self):
         return f"{self._nome}: {self._descricao}"
